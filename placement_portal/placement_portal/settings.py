02acfd07cc466aefed5ac4ce310063d8985024d4
Tanmoy07tech <Tanmoy07tech@users.noreply.github.com>
2021-08-24 20:15:42 +0530

    created the homepage

--- placement_portal/placement_portal/settings.py
+++ placement_portal/placement_portal/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'placement_portal.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
